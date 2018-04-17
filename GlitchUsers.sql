e393c77e912ef719c8de49417ef15ac122057dc7
Emily Jin <jin914@usc.edu>
2018-04-17 01:14:22 -0700

    updated sql

--- GlitchUsers.sql
+++ GlitchUsers.sql
@@ -19,13 +19,14 @@ CREATE TABLE Images(
 
 	imageID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userID INT(11) NOT NULL,
-    imageName VARCHAR(50) NOT NULL,
+    imageName VARCHAR(500) NOT NULL,
+    projectName VARCHAR(50) NOT NULL,
 	FOREIGN KEY fk1(userID) REFERENCES Users(userID)
     
 );
 
-INSERT INTO Images (userID, imageName)
-	VALUES  (1, 'image1');
+INSERT INTO Images (userID, imageName, projectName)
+	VALUES  (1, 'C:\\Users\\Emily Jin\\eclipse-workspace\\GlitchBrush\\photo.jpg', 'testImage');
     
     
     
