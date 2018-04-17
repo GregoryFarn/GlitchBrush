3972e92694e4982109b891d03ec777b67956a4cc
GregoryFarn <farn@usc.edu>
2018-04-17 00:04:59 -0700

    hmm

--- src/application/WorkBenchPage.java
+++ src/application/WorkBenchPage.java
@@ -195,6 +195,8 @@ public class WorkBenchPage extends Application {
         root.getChildren().add(workFrame);
         root.getChildren().add(selectedImage);
         Scene scene = new Scene(root, 1080, 600);
+        photo p = new photo(root, scene, "photoWide.jpg");
+		selectionBox sb = new selectionBox(root, scene, p);
         primaryStage.setScene(scene);
         java.net.URL url = this.getClass().getResource("WorkBenchPage.css");
         if (url == null) {
