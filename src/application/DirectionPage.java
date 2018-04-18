1c0c6724bff26bd00a980d6f169b4113b8809bf4
Size Shen <sizeshen@usc.edu>
2018-04-18 13:31:36 -0700

    direction page accessibility

--- src/application/DirectionPage.java
+++ src/application/DirectionPage.java
@@ -1,18 +1,25 @@
 package application;
 
 import javafx.scene.text.Font;
+
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
  
 public class DirectionPage extends Application {
-    public static void main(String[] args) {
+    static boolean accCheck = false;
+	public static void main(String[] args) {
         launch(args);
     }
     
@@ -34,6 +41,17 @@ public class DirectionPage extends Application {
                 s.start(primaryStage);
             }
         });
+        suli.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				if (accCheck) {
+				Media sound = new Media(new File("signuporlogin.mp3").toURI().toString());
+				MediaPlayer mp = new MediaPlayer(sound);
+				mp.play();}
+			}
+        });
         
         //Import Photo Button
         Hyperlink importPhoto = new Hyperlink("Import Photo");
@@ -48,10 +66,37 @@ public class DirectionPage extends Application {
                   p.start(primaryStage);
              }
         });
+        importPhoto.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				if (accCheck) {
+				Media sound = new Media(new File("importphoto.mp3").toURI().toString());
+				MediaPlayer mp = new MediaPlayer(sound);
+				mp.play();}
+			}
+        });
+        
+      //accessibility button
+       	Hyperlink acc = new Hyperlink("Accessibility");
+        acc.setId("share");
+        acc.setTranslateY(280);
+        acc.setTranslateX(-480);
+        acc.setBorder(Border.EMPTY);
+        acc.setTextFill(Color.BLACK);
+        acc.setOnAction(new EventHandler<ActionEvent>() {
+        	 
+            @Override
+            public void handle(ActionEvent event) {
+            	accCheck = !accCheck;
+            }
+        });
         
         StackPane root = new StackPane();
         root.getChildren().add(suli);
         root.getChildren().add(importPhoto);
+        root.getChildren().add(acc);
         Scene scene = new Scene(root, 1080, 600);
         primaryStage.setScene(scene);
         java.net.URL url = this.getClass().getResource("DirectionPage.css");
